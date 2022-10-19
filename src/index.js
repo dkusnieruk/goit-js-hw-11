5c60da156e95f6a0298f0844729310df0c081933
dkusnieruk <dariusz.kusnieruk@wp.pl>
2022-10-19 18:13:01 +0200

    update task

--- src/index.js
+++ src/index.js
@@ -1,2 +1,30 @@
+import axios from "axios"
+import Notiflix from 'notiflix';
 //comment to save
-console.log('connection');
+const API_KEY = '30699126-723906f358b47efc488aca811';
+
+const getInput = document.querySelector(`input`);
+let inputValue;
+getInput.addEventListener(`input`, (event)=>{
+    inputValue = event.target.value;
+    console.log(inputValue);
+    return inputValue;
+
+});
+
+const getButton =document.querySelector(`button`);
+
+getButton.addEventListener(`click`, (event)=>{
+    event.preventDefault();
+    Notiflix.Notify.failure('Qui timide rogat docet negare');
+    axios.get(`https://pixabay.com/api/key=${API_KEY}&q=${inputValue}&image_type=photo`)
+    .then(response =>{
+        response.request;
+        console.log(response);
+    })
+    .catch(error=>{
+        console.log(error);
+    })
+
+
+});
